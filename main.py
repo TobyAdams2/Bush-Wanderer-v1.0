d36e438a91367515257a87db986a596b33612974
Toby Adams <>
2021-05-13 08:48:10 +0000

    Version after one lesson to include basic UI, Colours of characters and clearly identify rules. Also the ability to set board size.

--- main.py
+++ main.py
@@ -1,27 +1,62 @@
 import random
 from time import sleep
+import os
 
 #this is just like a rules thing, the ---- thing is there to break up the rules into parts, just for readability and the look i suppose.
 print("~~~~~~Bush Wanderer~~~~~~")
-sleep(1)
-print("the rules of the game are as follows:")
+sleep(2)
+print("The rules of the game are as follows:")
+sleep(0.2)
 print("-------------------------------------")
-sleep(1)
+sleep(2)
 print("1. You are a bush wanderer")
-print("- your position on the board is marked by '#'")
+sleep(0.2)
+print("- Your position on the board is marked by \033[1;32mx\033[0;0m")
+sleep(0.2)
 print("-------------------------------------")
-sleep(1)
+sleep(2)
 print("2. On the board there is a secret chest.")
-print("- you must work your way around the board to reach the chest.")
+sleep(0.2)
+print("- You must work your way around the board to reach the chest.")
+sleep(0.2)
 print("- However, this chest is hidden from you, it is not marked on the board.")
-print("- if and when you land on the chest, you will win ad the game will end.")
-print
+sleep(0.2)
+print("- If and when you land on the chest, you will win and the game will end.")
+sleep(0.2)
+print("-------------------------------------")
+sleep(2)
+print("3. Also on the board with you is a blind but dangerous dragon")
+sleep(0.2)
+print("- This dragon is represented by the symbol \033[1;31m@\033[0;0m")
+sleep(0.2)
+print("- This dragon will randomly teleport around the board.")
+sleep(0.2)
+print("- If the dragon lands on your position you will die and then the game will end.")
+sleep(0.2)
+print("-------------------------------------")
+sleep(2)
+print("4. You can move in 4 directions:")
+sleep(0.2)
+print("- North, East, South, West.")
+sleep(0.2)
+print("- You control yourself using W, A, S and D. If you put the same input twice you will sprint and move two spaces instead of one.")
+sleep(2)
+print("Good luck wanderer....")
+sleep(0.2)
+print("-------------------------------------")
+sleep(0.2)
+print("loading....")
+sleep(0.2)
+print("-------------------------------------")
+print("")
+sleep(2.5)
+sleep(4)
+os.system("clear")
 
 #allows the player to set how large the map is
-grid = int(input("How large do you want the map to be? (Input a number. If you put in 5 the grid will be 5x5, etc."))
+grid = int(input("How large do you want the map to be? Input a number. If you put in 5 the grid will be 5x5, etc."))
 
 moves = 0
-
 x = 0
 y = 0
 
@@ -41,7 +76,7 @@ while(True):
       if i == dy and i == y:
         for u in range(grid):
           if u == dx:
-            print("\033[1;31m@ \033[0;0m", end="!")
+            print("\033[1;31m@ \033[0;0m", end="")
           elif u ==  x:
             print("\033[1;32mx \033[0;0m", end="")
           else:
@@ -95,6 +130,6 @@ while(True):
   elif m == "aa" and  x >0:
        x = x-2
   else:
-      print("Incorrect move")
+      print("Invalid move")
 
   moves = moves + 1
